5d25793a91a792e7d0a249531d759554b75d0475
Siya <89587223+siyabongaf018@users.noreply.github.com>
2024-01-18 11:35:24 +0200

    Delete Student-Attendance/Student_Attendance directory

--- Student-Attendance/Student_Attendance/src/main/java/com/example/Student_Attendance/Repository/DatesAttendedRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Student_Attendance.Repository;
-
-import com.example.Student_Attendance.Entity.DatesAttended;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DatesAttendedRepository extends JpaRepository<DatesAttended, Long> {
-}
--- Student-Attendance/Student_Attendance/src/main/java/com/example/Student_Attendance/Repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Student_Attendance.Repository;
-
-import com.example.Student_Attendance.Entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Long> {
-}
